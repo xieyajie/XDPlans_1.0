08e08afa10f16b1d57a7548cb3b039b431f10163
xieyajie <xyjxyf@163.com>
2013-09-25 17:57:15 +0800

    自定义菜单控件

--- XDPlans/Classes/All_plans/XDAllPlansViewController.m
+++ XDPlans/Classes/All_plans/XDAllPlansViewController.m
@@ -11,6 +11,8 @@
 #import "XDAllPlansCell.h"
 #import "XDNewPlanViewController.h"
 #import "XDPlanDetailViewController.h"
+
+#import "REMenu.h"
 #import "XDPlanLocalDefault.h"
 
 #define KPLANS_INDEX @"index"
@@ -32,12 +34,14 @@
 }
 
 @property (nonatomic, strong) UIView *tableHeaderView;
+@property (nonatomic, strong) REMenu *menu;
 
 @end
 
 @implementation XDAllPlansViewController
 
 @synthesize tableHeaderView = _tableHeaderView;
+@synthesize menu = _menu;
 
 - (id)initWithStyle:(UITableViewStyle)style
 {
@@ -303,11 +307,69 @@
     self.navigationItem.rightBarButtonItem = _createItem;
 }
 
+#pragma mark - get
+
+- (REMenu *)menu
+{
+    if (_menu == nil) {
+        REMenuItem *homeItem = [[REMenuItem alloc] initWithTitle:@"想做的事"
+                                                        subtitle:@"有很多想做的事情，先来几件排个队"
+                                                           image:[UIImage imageNamed:@"menu_allPlans.png"]
+                                                highlightedImage:nil
+                                                          action:^(REMenuItem *item) {
+                                                              NSLog(@"Item: %@", item);
+                                                          }];
+        
+        REMenuItem *exploreItem = [[REMenuItem alloc] initWithTitle:@"正在进行的事"
+                                                           subtitle:@"贪多嚼不烂哦，专心做一件事情吧"
+                                                              image:[UIImage imageNamed:@"menu_actionPlan.png"]
+                                                   highlightedImage:nil
+                                                             action:^(REMenuItem *item) {
+                                                                 NSLog(@"Item: %@", item);
+                                                             }];
+        
+        REMenuItem *activityItem = [[REMenuItem alloc] initWithTitle:@"今天的计划"
+                                                            subtitle:@"想做些什么呢？坚持进行下去吧"
+                                                               image:[UIImage imageNamed:@"menu_todayPlan.png"]
+                                                    highlightedImage:nil
+                                                              action:^(REMenuItem *item) {
+                                                                  NSLog(@"Item: %@", item);
+                                                              }];
+        
+        REMenuItem *profileItem = [[REMenuItem alloc] initWithTitle:@"设置"
+                                                              image:[UIImage imageNamed:@"menu_setting.png"]
+                                                   highlightedImage:nil
+                                                             action:^(REMenuItem *item) {
+                                                                 NSLog(@"Item: %@", item);
+                                                             }];
+        
+        homeItem.tag = 0;
+        exploreItem.tag = 1;
+        activityItem.tag = 2;
+        profileItem.tag = 3;
+        
+        _menu = [[REMenu alloc] initWithItems:@[homeItem, exploreItem, activityItem, profileItem]];
+        _menu.cornerRadius = 4;
+        _menu.shadowColor = [UIColor blackColor];
+        _menu.shadowOffset = CGSizeMake(0, 1);
+        _menu.shadowOpacity = 1;
+        _menu.imageOffset = CGSizeMake(5, -1);
+    }
+    
+    return _menu;
+}
+
 #pragma mrk - item/button action
 
 - (void)menuAction:(id)sender
 {
+    if (self.menu.isOpen)
+    {
+        return [self.menu close];
+    }
     
+    [self.menu showFromNavigationController:self.navigationController];
+
 }
 
 - (void)createEvent:(id)sender
